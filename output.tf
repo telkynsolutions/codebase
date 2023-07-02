ae4e11f163b3f1d79858ecf6367580147dbed61c
telkynsolutions <53535763+telkynsolutions@users.noreply.github.com>
2023-07-02 10:48:26 +0100

    Changes made to ouput

--- output.tf
+++ output.tf
@@ -1,6 +1,6 @@
 output "ec2_amazon_Linux_ip" {
   value = aws_instance.ec2_amazon_Linux.public_ip
 }
-output "ec2_ubuntu_ip" {
+/**output "ec2_ubuntu_ip" {
   value = aws_instance.ec2_ubuntu.public_ip
-}
+}**/
