df2470610fd86143590db652c9e1fd14616c3795
telkynsolutions <53535763+telkynsolutions@users.noreply.github.com>
2023-07-06 22:35:46 +0100

    Removed unwanted code

--- variable.tf
+++ variable.tf
@@ -4,12 +4,6 @@ variable "ec2_ami" {
   default     = "ami-04b1c88a6bbd48f8e"
 }
 
-/**variable "Ubuntu_ec2_ami" {
-  description = "This is a variable to manage AMI"
-  type        = string
-  default     = "ami-07c6e43366b19b83c"
-}**/
-
 variable "ec2_key_name" {
   description = "This is a variable to manage ec2 key name"
   type        = string
@@ -21,18 +15,6 @@ variable "ec2_instance_type" {
   type        = string
   default     = "t2.micro"
 }
-
-/**variable "ubuntu_tag" {
-  description = "This is a variable to manage ec2 tags"
-  type        = map(any)
-  default = {
-    "Name" = "Ubuntu Linux node"
-    "Dept" = "Development"
-    "Faculty" = "Infrastructure"
-    "Can destroy" = "Yes"
-  }
-}**/
-
 
 variable "default_tags" {
   description = "Additional resource tags"
